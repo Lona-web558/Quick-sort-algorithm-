a125c9c79396f7b0d509994ea72ecfabc677b715
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2024-07-23 13:00:58 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int partitionIndex = Partition(arr, low, high);
+
+            QuickSort(arr, low, partitionIndex - 1);
+            QuickSort(arr, partitionIndex + 1, high);
+        }
+    }
+
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                Swap(arr, i, j);
+            }
+        }
+
+        Swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    static void Swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 10, 7, 8, 9, 1, 5 };
+
+        Console.WriteLine("Original array:");
+        PrintArray(numbers);
+
+        QuickSort(numbers, 0, numbers.Length - 1);
+
+        Console.WriteLine("Sorted array:");
+        PrintArray(numbers);
+    }
+}
